ffadc3fa6f6920a50982842edfe92172e06b3072
CI <mihaigalos@gmail.com>
2021-03-14 16:24:26 +0100

    Add results for Go+Rust to generated CSV

--- dot_product/optimized_with_rust/main.go
+++ dot_product/optimized_with_rust/main.go
@@ -21,15 +21,14 @@ func randomSlice() []int32 {
 
 func main() {
 	t1 := time.Now()
-	fmt.Println("generating random slices..")
 	s1 := randomSlice()
 	s2 := randomSlice()
 	t2 := time.Now()
-	fmt.Println(t2.Sub(t1))
+	fmt.Print(t2.Sub(t1), ", ")
 
-	fmt.Println("starting..")
 	t3 := time.Now()
 	result := make([]int32, len(s1), len(s1))
 	C.dot_product((*C.int)(&s1[0]), (*C.int)(&s2[0]), C.int(len(s1)), (*C.int)(&result[0]))
-	fmt.Println(t3.Sub(t2))
+	fmt.Print(t3.Sub(t2), ", ")
+	fmt.Println("Go+Rust")
 }

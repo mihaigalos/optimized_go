4c0d7e928bde08c75223a3d3609bfb48c2030a16
CI <mihaigalos@gmail.com>
2021-03-14 10:34:23 +0100

    Specify length of datatype to smooth transition to optimization

--- dot_product/pure_go.go
+++ dot_product/pure_go.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func Parallelize(functions ...func()) {
+func parallelize(functions ...func()) {
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(len(functions))
 
@@ -21,15 +21,15 @@ func Parallelize(functions ...func()) {
 	}
 }
 
-func randomMap() map[int]int {
-	m := make(map[int]int)
-	for i := 0; i < 50000000; i++ {
-		m[i] = rand.Intn(100000)
+func randomMap() map[uint32]int32 {
+	m := make(map[uint32]int32)
+	for i := uint32(0); i < 50000000; i++ {
+		m[i] = rand.Int31n(100000)
 	}
 	return m
 }
-func parallelProduct(m1 *map[int]int, m2 *map[int]int, start int, stop int) map[int]int {
-	result := make(map[int]int)
+func parallelProduct(m1 *map[uint32]int32, m2 *map[uint32]int32, start uint32, stop uint32) map[uint32]int32 {
+	result := make(map[uint32]int32)
 	for i := start; i < stop; i++ {
 		result[i] = (*m1)[i] * (*m2)[i]
 	}
@@ -44,26 +44,26 @@ func main() {
 	t2 := time.Now()
 	fmt.Println(t2.Sub(t1))
 
-	result1 := make(map[int]int)
-	result2 := make(map[int]int)
-	result3 := make(map[int]int)
-	result4 := make(map[int]int)
+	result1 := make(map[uint32]int32)
+	result2 := make(map[uint32]int32)
+	result3 := make(map[uint32]int32)
+	result4 := make(map[uint32]int32)
 
 	fmt.Println("starting..")
 	func1 := func() {
-		result1 = parallelProduct(&m1, &m2, 0, len(m1)/4)
+		result1 = parallelProduct(&m1, &m2, 0, uint32(len(m1)/4))
 	}
 	func2 := func() {
-		result2 = parallelProduct(&m1, &m2, len(m1)/4+1, len(m1)/2)
+		result2 = parallelProduct(&m1, &m2, uint32(len(m1)/4+1), uint32(len(m1)/2))
 	}
 	func3 := func() {
-		result3 = parallelProduct(&m1, &m2, len(m1)/2+1, len(m1)/2+len(m1)/4)
+		result3 = parallelProduct(&m1, &m2, uint32(len(m1)/2+1), uint32(len(m1)/2+len(m1)/4))
 	}
 	func4 := func() {
-		result4 = parallelProduct(&m1, &m2, len(m1)/2+len(m1)/4+1, len(m1))
+		result4 = parallelProduct(&m1, &m2, uint32(len(m1)/2+len(m1)/4+1), uint32(len(m1)))
 	}
 	fmt.Println("parallelizing..")
-	Parallelize(func1, func2, func3, func4)
+	parallelize(func1, func2, func3, func4)
 	t3 := time.Now()
 	fmt.Println(t3.Sub(t2))
 
